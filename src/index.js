3c9cf4cfc143cf3f8aa4ccbce3ef9aa68bf47f33
tecnomage <tecnocratay@gmail.com>
2020-07-28 10:15:25 -0300

    using react router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+
+
+function CadastroVideo()
+{
+ return (
+    <div>
+      Cadastro de videos na plataforma
+    </div>
+ )
+}
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+<BrowserRouter>
+<Switch>
+<Route path="/" component={App}/>
+<Route path="/cadastro" component={CadastroVideo}/>
+
+</Switch>
+</BrowserRouter>,
+
   document.getElementById('root')
 );
 
