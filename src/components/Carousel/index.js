8ae034f3849e61f65c5ad6897686ed5b38fe684c
tecnomage <tecnocratay@gmail.com>
2020-07-28 17:30:41 -0300

    carousel ok

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
-import VideoCard from './VideoCard';
-import Slider, { SliderItem } from './Slider';
+import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
 
 
 function Carousel({
