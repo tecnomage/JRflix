7255cabbf4f7e6ddaf64798050c86616b3b09825
tecnomage <tecnocratay@gmail.com>
2020-07-30 15:21:19 -0300

    creating fake api

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -37,6 +37,10 @@ function CadastroCategoria() {
     setValue(name, value);
   }
 
+  useEffect(() => {
+    console.log('shoooooooooow');
+  });
+
   return (
     <PageDefault>
       <h1>
@@ -69,7 +73,7 @@ function CadastroCategoria() {
         />
         <Button>Cadastrar</Button>
       </form>
-
+      {categorias.length === 0 && (<div>Loading.....</div>)}
       <ul>
         {categorias.map((categoria, i) => {
           console.log(categoria);
