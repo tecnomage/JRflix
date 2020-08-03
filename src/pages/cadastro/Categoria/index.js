51a4eba4a900a739ea995d64e96f0c351d92022a
tecnomage <tecnocratay@gmail.com>
2020-08-03 09:26:52 -0300

    finished useCustomForms

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,8 +7,9 @@ import Button from '../../../components/Button';
 
 function UseForm(valoresIniciais) {
   const [values, setvalues] = useState(valoresIniciais);
-  
+
   function setValue(chave, valor) {
+    console.log('setValues');
     setvalues({
       ...values,
       [chave]: valor,
@@ -17,19 +18,19 @@ function UseForm(valoresIniciais) {
   function handleChange(infosDoEvento) {
     const name = infosDoEvento.target.getAttribute('name');
     const { value } = infosDoEvento.target;
-
+    console.log('handleChange');
     setValue(name, value);
   }
 
   function clearForm() {
     setvalues(valoresIniciais);
-
+    console.log('clear');
   }
 
   return {
     values,
     handleChange,
-    clearForm
+    clearForm,
   };
 }
 
@@ -55,7 +56,7 @@ function CadastroCategoria() {
   function handleSubmit(infosDoEvento) {
     infosDoEvento.preventDefault();
     setCategorias([...categorias, values]);
-    
+
     clearForm();
   }
 
@@ -65,8 +66,8 @@ function CadastroCategoria() {
     //   .then((respostaDoServidor) => respostaDoServidor.json())
     //   .then((dados) => setCategorias([...dados]));
 
-    //const URL_TOP = 'http://localhost:8080/categorias';
-    const URL_TOP = 'https://jrflix.herokuapp.com/categorias';
+    const URL_TOP = 'http://localhost:8080/categorias';
+    // const URL_TOP = 'https://jrflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
@@ -81,15 +82,15 @@ function CadastroCategoria() {
     <PageDefault>
       <h1>
         Cadastro de Categoria :
-        {values.nome}
+        {values.titulo}
       </h1>
 
       <form onSubmit={handleSubmit}>
         <FormField
           label="Nome da categoria"
           type="input"
-          value={values.nome}
-          name="nome"
+          value={values.titulo}
+          name="titulo"
           handleChange={handleChange}
         />
         <FormField
@@ -111,7 +112,9 @@ function CadastroCategoria() {
       </form>
       {categorias.length === 0 && (<div>Loading.....</div>)}
       <ul>
-        {categorias.map((categoria, i) => <li key={i}>{categoria.titulo}</li>)}
+  {categorias.map((categoria, i) => {
+  //console.log(categoria)
+  return <li key={i}>{categoria.titulo}</li>})}
       </ul>
 
       <Link to="/">Ir para home</Link>
